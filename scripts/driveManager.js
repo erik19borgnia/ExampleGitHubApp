a7411d4e285181fc0ee201d5621cd7b2f99d1608
erik19borgnia <erik19borgnia@gmail.com>
2025-12-08 03:13:55 -0300

    More fixes...

--- scripts/driveManager.js
+++ scripts/driveManager.js
@@ -1,6 +1,9 @@
 function gapiLoaded() {
-    gapi.load('client', initializeGapiClient)
-    
+    document.getElementById("exportBtnDrive").classList.remove("hidden")
+    document.getElementById("importBtnDrive").classList.remove("hidden")
+    document.getElementById("exportBtnDrive").addEventListener("click", () => exportToDrive())
+    document.getElementById("importBtnDrive").addEventListener("click", () => importFromDrive())
+    gapi.load('client', initializeGapiClient)    
 }
 
 async function initializeGapiClient() {
@@ -10,27 +13,23 @@ async function initializeGapiClient() {
             clientId: '253006367900-afh5cqbmqhuvse3n6grt0hch5tahinu7.apps.googleusercontent.com',
             scope: 'https://www.googleapis.com/auth/drive', // Or more specific scopes like drive.file
             discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-        });
-        gapi.client.setApiKey('AIzaSyD0J6RzL4kQHee5EOLAjNZROXj6wIeNCqs'); // If using an API key
+        })
+        gapi.client.setApiKey('AIzaSyD0J6RzL4kQHee5EOLAjNZROXj6wIeNCqs') // If using an API key
         // Handle user sign-in/out
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus)
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get())
         console.log("Gapi initialized!")
-        document.getElementById("exportBtnDrive").classList.remove("hidden")
-        document.getElementById("importBtnDrive").classList.remove("hidden")
-        document.getElementById("exportBtnDrive").addEventListener("click", () => exportToDrive())
-        document.getElementById("importBtnDrive").addEventListener("click", () => importFromDrive())
     }catch(error){
         console.error(error)
     }
 }
 
 function handleAuthClick() {
-    gapi.auth2.getAuthInstance().signIn();
+    gapi.auth2.getAuthInstance().signIn()
 }
 
 function handleSignoutClick() {
-    gapi.auth2.getAuthInstance().signOut();
+    gapi.auth2.getAuthInstance().signOut()
 }
 
 function exportToDrive(){
@@ -39,5 +38,6 @@ function exportToDrive(){
 }
 
 function importFromDrive(){
+    handleSignoutClick()
     console.log("Import!")
 }
