8f5440d9e0f3f86edff5af528b5710580c865c44
erik19borgnia <erik19borgnia@gmail.com>
2025-12-08 03:00:23 -0300

    Try catch

--- scripts/driveManager.js
+++ scripts/driveManager.js
@@ -4,21 +4,25 @@ function gapiLoaded() {
 }
 
 async function initializeGapiClient() {
-    await gapi.client.init({
-        apiKey: 'AIzaSyD0J6RzL4kQHee5EOLAjNZROXj6wIeNCqs', // Optional, if using only OAuth
-        clientId: '253006367900-afh5cqbmqhuvse3n6grt0hch5tahinu7.apps.googleusercontent.com',
-        scope: 'https://www.googleapis.com/auth/drive', // Or more specific scopes like drive.file
-        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-    });
-    gapi.client.setApiKey('AIzaSyD0J6RzL4kQHee5EOLAjNZROXj6wIeNCqs'); // If using an API key
-    // Handle user sign-in/out
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    console.log("Gapi initialized!")
-    document.getElementById("exportBtnDrive").classList.remove("hidden")
-    document.getElementById("importBtnDrive").classList.remove("hidden")
-    document.getElementById("exportBtnDrive").addEventListener("click", () => exportToDrive())
-    document.getElementById("importBtnDrive").addEventListener("click", () => importFromDrive())
+    try{
+        await gapi.client.init({
+            apiKey: 'AIzaSyD0J6RzL4kQHee5EOLAjNZROXj6wIeNCqs', // Optional, if using only OAuth
+            clientId: '253006367900-afh5cqbmqhuvse3n6grt0hch5tahinu7.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/drive', // Or more specific scopes like drive.file
+            discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+        });
+        gapi.client.setApiKey('AIzaSyD0J6RzL4kQHee5EOLAjNZROXj6wIeNCqs'); // If using an API key
+        // Handle user sign-in/out
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        console.log("Gapi initialized!")
+        document.getElementById("exportBtnDrive").classList.remove("hidden")
+        document.getElementById("importBtnDrive").classList.remove("hidden")
+        document.getElementById("exportBtnDrive").addEventListener("click", () => exportToDrive())
+        document.getElementById("importBtnDrive").addEventListener("click", () => importFromDrive())
+    }catch(error){
+        console.error(error)
+    }
 }
 
 function handleAuthClick() {
